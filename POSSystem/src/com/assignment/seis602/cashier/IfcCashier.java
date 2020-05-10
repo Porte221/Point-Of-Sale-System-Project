b41e9178be9540580841b787c7eb67ebdc29059d
Jerome <Jerome@localhost>
2020-05-09 20:30:14 -0500

    Added login text file and included code to cashier class

---
+++ POSSystem/src/com/assignment/seis602/cashier/IfcCashier.java
@@ -0,0 +1,11 @@
+package com.assignment.seis602.cashier;
+import java.io.IOException;
+
+public interface IfcCashier {
+
+	 //This method verify the input from console
+	 public static void verifyLogin() throws IOException{
+		 
+	 }
+
+}
